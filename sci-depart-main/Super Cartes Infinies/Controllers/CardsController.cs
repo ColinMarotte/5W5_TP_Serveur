e9207c5394fd3cdccd311eeb8442a122dd8d8ae9
David Bourgeois <1192146@cegepmontpetit.ca>
2025-02-18 11:35:18 -0500

    FCT ajout de la contrainte du rôle d'administrateur pour CardsController
    Il faut maintenant être un administrateur pour pouvoir accéder au contrôleur.

--- sci-depart-main/Super Cartes Infinies/Controllers/CardsController.cs
+++ sci-depart-main/Super Cartes Infinies/Controllers/CardsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Super_Cartes_Infinies.Models;
 
 namespace Super_Cartes_Infinies.Controllers
 {
+    [Authorize(Roles= ApplicationDbContext.ADMIN_ROLE)]
     public class CardsController : Controller
     {
         private readonly ApplicationDbContext _context;
