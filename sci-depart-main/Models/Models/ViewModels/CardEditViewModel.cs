0a8b64220df517540536cbb713e9a91f0dbe1ba8
jihane <2282988@cegepmontpetit.ca>
2025-04-14 08:27:47 -0400

    FCT : CardEditViewModel

---
+++ sci-depart-main/Models/Models/ViewModels/CardEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Super_Cartes_Infinies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models.ViewModels
+{
+    public class CardEditViewModel
+    {
+        public Card Card { get; set; }
+        public List<CardPower> CardPowers { get; set; }
+        public List<Power> AllPowers { get; set; }
+
+        public int SelectedPowerId { get; set; }
+        public int? PowerValue { get; set; }
+    }
+}
